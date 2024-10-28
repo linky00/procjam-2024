a635205d98ae8e0f4c4209ddf901e12de8bb3445
note <omeraligencoglu7@gmail.com>
2024-10-28 13:02:12 -0400

    fix char creation

--- rust/src/world.rs
+++ rust/src/world.rs
@@ -8,17 +8,18 @@ pub struct CityID(pub u32);
 pub struct EventID(u32);
 
 #[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
-pub struct CharacterID(u32);
+pub struct CharacterID(pub u32);
 
 #[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
 pub struct Year(i32);
 
-const NUM_LAYERS: i32 = 5;
+const NUM_LAYERS: usize = 5;
+const NUM_CHARACTERS: usize = 3;
 
 pub struct World {
     pub cities: HashMap<CityID, City>,
     // events: HashMap<EventID, Event>,
-    characters: HashMap<CharacterID, Character>,
+    pub characters: HashMap<CharacterID, Character>,
     city_id_counter: u32,
     event_id_counter: u32,
     character_id_counter: u32,
@@ -30,6 +31,7 @@ impl World {
     pub fn new() -> Self {
         World {
             cities: HashMap::new(),
+            characters: HashMap::new(),
             city_id_counter: 0,
             event_id_counter: 0,
             character_id_counter: 0,
@@ -43,10 +45,10 @@ impl World {
        
         // add start and end cities
         world.layers[0] = vec![world.add_city(0)];
-        world.layers[4] = vec![world.add_city(4)];
+        world.layers[NUM_LAYERS - 1] = vec![world.add_city(NUM_LAYERS - 1)];
         
         // add in between cities
-        for layer in (1..=3) {
+        for layer in (1..=NUM_LAYERS - 2) {
             let num_cities = rand::thread_rng().gen_range(2..=4);
             for _ in (1..=num_cities) {
                 let new_city = world.add_city(layer);
@@ -61,6 +63,16 @@ impl World {
                 }
             }
         }
+
+        // add characters at their start positions (for now just the first city)
+        for _ in (0..NUM_CHARACTERS) {
+            let pns = Pronouns {
+                nominative: "pn1".to_string(),
+                accusative: "pn2".to_string(),
+                dep_genitive: "pn3".to_string()
+            };
+            let new_character = world.add_character("test".to_string(), pns, world.layers[0][0]);
+        }
         
         world
     }
@@ -74,14 +86,14 @@ impl World {
         CityID(id)
     }
 
-    fn add_character(&mut self, start_city: CityID) {
-        if (self.city_id_counter <= start_city) {
+    fn add_character(&mut self, name: String, pronouns: Pronouns, CityID(start_city_id): CityID) {
+        if (self.city_id_counter <= start_city_id) {
             // error
         }
 
         let id = self.character_id_counter;
         self.character_id_counter += 1;
-        let char = Character::new()
+        let char = Character::new(name, pronouns);
         self.characters.insert(CharacterID(id), char);
         CharacterID(id);
     }
@@ -103,17 +115,26 @@ impl City {
     }
 }
 
+#[derive(Debug)]
 pub struct Character {
     // used in textgen
     pub name: String,
-    pub pronouns: [String, 3], // 1: nominative (ex. she, him, they) 2: accusative (ex: her, him, them) 3: dependent genitive (ex: her, his, their)
+    pub pronouns: Pronouns,
     pub events: Vec<EventID>
 }
 
+#[derive(Debug)]
+pub struct Pronouns {
+    pub nominative: String, // she, him, they etc.
+    pub accusative: String, // her, him, them etc.
+    pub dep_genitive: String // her, his, their etc.
+}
+
 impl Character {
-    pub fn new() -> Self {
-        Person {
-            name: "".to_string(),
+    pub fn new(name: String, pronouns: Pronouns) -> Self {
+        Character {
+            name: name,
+            pronouns: pronouns,
             events: Vec::new()
         }
     }
