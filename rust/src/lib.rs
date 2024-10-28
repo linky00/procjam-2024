037340bb4c195914ec9b338aecc086a5e65518cb
note <omeraligencoglu7@gmail.com>
2024-10-28 16:35:08 -0400

    add event struct

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,17 +7,38 @@ mod tests {
     #[test]
     fn world_generator() {
         use crate::world::*;
-        
+
         let mut world = world::World::generate_world();
-        println!("{:?}",world.layers[0]);
-        println!("{:?}",world.layers[1]);
-        println!("{:?}",world.layers[2]);
-        println!("{:?}",world.layers[3]);
-        println!("{:?}",world.layers[4]);
-        println!("{:?}",world.characters[&CharacterID(0)]);
-        println!("{:?}",world.characters[&CharacterID(1)]);
-        println!("{:?}",world.characters[&CharacterID(2)]);
-    
+        println!("{:?}", world.layers[0]);
+        println!("{:?}", world.layers[1]);
+        println!("{:?}", world.layers[2]);
+        println!("{:?}", world.layers[3]);
+        println!("{:?}", world.layers[4]);
+        println!("{:?}", world.characters[&CharacterID(0)]);
+        println!("{:?}", world.characters[&CharacterID(1)]);
+        println!("{:?}", world.characters[&CharacterID(2)]);
+
         println!("{:?}", world.cities.get(&CityID(3)).unwrap().neighbours);
+    }
+
+    #[test]
+    fn events() {
+        use crate::world::*;
+
+        // try creating some discrete events
+        let test_event: Event =
+            Event::new(vec![world::CharacterID(0)], 2, None, EventType::EventDeath);
+
+        let test_event2: Event = Event::new(
+            vec![world::CharacterID(0)],
+            1,
+            Some(4),
+            EventType::EventDeath,
+        );
+
+        println!("{:?}", test_event);
+        println!("{:?}", test_event2);
+
+        // test_event.add_event_during(world, event_id);
     }
 }
--- rust/src/world.rs
+++ rust/src/world.rs
@@ -1,11 +1,11 @@
-use std::collections::HashMap;
 use rand::Rng;
+use std::collections::HashMap;
 
 #[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
 pub struct CityID(pub u32);
 
 #[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
-pub struct EventID(u32);
+pub struct EventID(pub u32);
 
 #[derive(Eq, Hash, PartialEq, Copy, Clone, Debug)]
 pub struct CharacterID(pub u32);
@@ -16,15 +16,18 @@ pub struct Year(i32);
 const NUM_LAYERS: usize = 5;
 const NUM_CHARACTERS: usize = 3;
 
+// -- World and world gen --
+
 pub struct World {
     pub cities: HashMap<CityID, City>,
     // events: HashMap<EventID, Event>,
     pub characters: HashMap<CharacterID, Character>,
+    pub events: HashMap<EventID, Event>,
     city_id_counter: u32,
     event_id_counter: u32,
     character_id_counter: u32,
-    
-    pub layers: [Vec<CityID>; NUM_LAYERS]
+
+    pub layers: [Vec<CityID>; NUM_LAYERS],
 }
 
 impl World {
@@ -32,32 +35,33 @@ impl World {
         World {
             cities: HashMap::new(),
             characters: HashMap::new(),
+            events: HashMap::new(),
             city_id_counter: 0,
             event_id_counter: 0,
             character_id_counter: 0,
-            
-            layers: [Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()]
+
+            layers: [Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()],
         }
     }
-    
+
     pub fn generate_world() -> Self {
         let mut world = World::new();
-       
+
         // add start and end cities
         world.layers[0] = vec![world.add_city(0)];
         world.layers[NUM_LAYERS - 1] = vec![world.add_city(NUM_LAYERS - 1)];
-        
+
         // add in between cities
         for layer in (1..=NUM_LAYERS - 2) {
             let num_cities = rand::thread_rng().gen_range(2..=4);
             for _ in (1..=num_cities) {
                 let new_city = world.add_city(layer);
                 world.layers[layer].push(new_city);
-                
-                for city_id in world.layers[layer-1].clone() {
+
+                for city_id in world.layers[layer - 1].clone() {
                     let mut prev_city = world.cities.get_mut(&city_id).unwrap();
                     prev_city.neighbours.push(new_city);
-                    
+
                     let mut new_city = world.cities.get_mut(&new_city).unwrap();
                     new_city.neighbours.push(city_id);
                 }
@@ -69,40 +73,47 @@ impl World {
             let pns = Pronouns {
                 nominative: "pn1".to_string(),
                 accusative: "pn2".to_string(),
-                dep_genitive: "pn3".to_string()
+                dep_genitive: "pn3".to_string(),
             };
-            let new_character = world.add_character("test".to_string(), pns, world.layers[0][0]);
+            world.add_character("test".to_string(), pns, world.layers[0][0]);
         }
-        
+
         world
     }
-    
+
     fn add_city(&mut self, layer: usize) -> CityID {
         let id = self.city_id_counter;
         self.city_id_counter += 1;
-        
+
         let city = City::new();
         self.cities.insert(CityID(id), city);
         CityID(id)
     }
 
-    fn add_character(&mut self, name: String, pronouns: Pronouns, CityID(start_city_id): CityID) {
+    fn add_character(
+        &mut self,
+        name: String,
+        pronouns: Pronouns,
+        CityID(start_city_id): CityID,
+    ) -> CharacterID {
         if (self.city_id_counter <= start_city_id) {
-            // error
+            panic!("Invalid start city ID: {}", start_city_id); // invalid id
         }
 
         let id = self.character_id_counter;
         self.character_id_counter += 1;
         let char = Character::new(name, pronouns);
         self.characters.insert(CharacterID(id), char);
-        CharacterID(id);
+        CharacterID(id)
     }
 }
+
+// -- City class --
 
 pub struct City {
     pub name: String,
     pub neighbours: Vec<CityID>,
-    pub events: Vec<EventID>
+    pub events: Vec<EventID>,
 }
 
 impl City {
@@ -110,24 +121,28 @@ impl City {
         City {
             name: "".to_string(),
             neighbours: Vec::new(),
-            events: Vec::new()
+            events: Vec::new(),
         }
     }
 }
+
+// -- Character class and associated classes
 
 #[derive(Debug)]
 pub struct Character {
     // used in textgen
     pub name: String,
     pub pronouns: Pronouns,
-    pub events: Vec<EventID>
+    pub events: Vec<EventID>,
 }
 
 #[derive(Debug)]
 pub struct Pronouns {
+    // probably gonna be needed for text gen
     pub nominative: String, // she, him, they etc.
     pub accusative: String, // her, him, them etc.
-    pub dep_genitive: String // her, his, their etc.
+    pub dep_genitive: String, // her, his, their etc.
+                            // could add more if needed
 }
 
 impl Character {
@@ -135,7 +150,82 @@ impl Character {
         Character {
             name: name,
             pronouns: pronouns,
-            events: Vec::new()
+            events: Vec::new(),
+        }
+    }
+}
+
+// -- Events --
+
+// event types
+#[derive(Debug)]
+pub enum EventType {
+    EventDeath,        // an event representing the death of a character.
+    EventEncounter, // an event representing a fleeting encounter between two people. An alive character could encounter a dead character.
+    EventMoveTogether, // an event representing two characters moving together for a while.
+}
+
+// An event that has a start time and maybe an end time.
+#[derive(Debug)]
+pub struct Event {
+    characters: Vec<CharacterID>, // characters in the event
+    start_time: usize,
+    end_time: Option<usize>,
+    event_type: EventType,
+    events_happening_during: Vec<EventID>,
+}
+
+// Helper function to check if two event times overlap
+pub fn durations_overlap(
+    start1: usize,
+    start2: usize,
+    end1: Option<usize>,
+    end2: Option<usize>,
+) -> bool {
+    if (start1 == start2) {
+        true;
+    }
+    match (end1, end2) {
+        (None, None) => false,
+        (Some(e1), None) => start2 > start1 && start2 < e1,
+        (None, Some(e2)) => start1 > start2 && start1 < e2,
+        (Some(e1), Some(e2)) => !(start1 > e2 || e1 < start2),
+    }
+}
+
+impl Event {
+    pub fn new(
+        characters: Vec<CharacterID>,
+        start_time: usize,
+        end_time: Option<usize>,
+        event_type: EventType,
+    ) -> Self {
+        Event {
+            characters,
+            start_time,
+            end_time,
+            event_type,
+            events_happening_during: Vec::new(),
+        }
+    }
+
+    // TODO: to be tested when events are added into world gen
+    pub fn add_event_during(&mut self, world: World, event_id: EventID) {
+        match world.events.get(&event_id) {
+            Some(&ref event) => {
+                let valid: bool = durations_overlap(
+                    self.start_time,
+                    event.start_time,
+                    self.end_time,
+                    event.end_time,
+                );
+                if valid {
+                    self.events_happening_during.push(event_id);
+                }
+            }
+            None => match event_id {
+                EventID(id) => panic!("No event found associated with eventID {}", id),
+            },
         }
     }
 }
