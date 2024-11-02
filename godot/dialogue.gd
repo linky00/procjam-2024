59a380deaabf9b86e6a8890a54b163d21ebe66ba
halcyon3d <99849062+halcyon3d@users.noreply.github.com>
2024-11-02 18:42:36 +0000

    added movement

--- godot/dialogue.gd
+++ godot/dialogue.gd
@@ -1,24 +1,25 @@
 extends Control
 
-# @onready var history = $History
+@onready var history = $History
 
 @onready var textbox = get_node("TextArea/TextBox")
 @onready var text = [ "Welcome to my shop!",  "Feel free to look around."]
 @onready var i = 0
 
 func _ready():
-	# history.generate_history()
-	# var item = history.get_item(0)
-	# print(item.item_type)
-	# print(item.description)
-	# for story in item.stories:
-	# 	print(story.lines)
+	history.generate_history()
+	var item = history.get_item(0)
+	print(item.item_type)
+	print(item.description)
+	for story in item.stories:
+		print(story.lines)
 	
-	pass
+	textbox.text = text[0]
 
 func _input(event):
-	if event.is_action_pressed("mouse_click"):
+	if event.is_action_pressed("mouse_click") and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		var item = history.get_item(0)
 		i += 1
-		if i >= len(text):
+		if i >= len(item.stories[-1].lines):
 			i = 0
-		textbox.text = text[i]
+		textbox.text = item.stories[-1].lines[i]
---
+++ godot/movement.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+var mouse_sensitivity = 0.002
+var speed = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	var input = Input.get_vector("left", "right", "forward", "backward")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+	
+	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+	if event.is_action_pressed("escape"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event.is_action_pressed("mouse_click"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
