b56290e26f6769e671dfea64f4ac2df6cb962035
halcyon3d <99849062+halcyon3d@users.noreply.github.com>
2024-11-02 16:50:57 +0000

    added shopkeeper and text changing in ui

--- godot/dialogue.gd
+++ godot/dialogue.gd
@@ -2,11 +2,23 @@ extends Control
 
 # @onready var history = $History
 
+@onready var textbox = get_node("TextArea/TextBox")
+@onready var text = [ "Welcome to my shop!",  "Feel free to look around."]
+@onready var i = 0
+
 func _ready():
-	pass
 	# history.generate_history()
 	# var item = history.get_item(0)
 	# print(item.item_type)
 	# print(item.description)
 	# for story in item.stories:
 	# 	print(story.lines)
+	
+	pass
+
+func _input(event):
+	if event.is_action_pressed("mouse_click"):
+		i += 1
+		if i >= len(text):
+			i = 0
+		textbox.text = text[i]
