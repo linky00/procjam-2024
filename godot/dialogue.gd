446ca75c5cb06b2e6ead51de523d77a5219da79b
halcyon3d <99849062+halcyon3d@users.noreply.github.com>
2024-11-02 16:22:36 +0000

    created menu and started to work on ui/frontend

--- godot/dialogue.gd
+++ godot/dialogue.gd
@@ -1,11 +1,12 @@
 extends Control
 
-@onready var history = $History
+# @onready var history = $History
 
 func _ready():
-	history.generate_history()
-	var item = history.get_item(0)
-	print(item.item_type)
-	print(item.description)
-	for story in item.stories:
-		print(story.lines)
+	pass
+	# history.generate_history()
+	# var item = history.get_item(0)
+	# print(item.item_type)
+	# print(item.description)
+	# for story in item.stories:
+	# 	print(story.lines)
